e5cce61639ce71b0bdea2e6de1fc7093abe7fcb1
adamanapua <Awinsmith27@gmail.com>
2020-02-06 22:13:52 -1000

    first commit

---
+++ test.js
@@ -0,0 +1,8 @@
+function getTip () {
+    var percent = document.getElementById("percent-tip").value;
+    var dollar = document.getElementById("dollar-amount").value;
+    var decimalpercent = percent / 100;
+    var tip = decimalpercent * dollar;
+    alert("The tip is " + "$" + tip);
+
+}
